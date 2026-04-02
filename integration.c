89fd3d95ff5fc69a19379c5fcef01c3955c8db8d
MVT2911 <mvarasu1999@gmail.com>
2026-04-02 00:13:51 -0300

    Adding more results to print

--- integration.c
+++ integration.c
@@ -175,6 +175,8 @@ int main(int argc, char** argv) {
         printf("\n--- Results (Mode %d) ---\n", mode);
         printf("Integral: %.10f\n", total_integral);
         printf("Total Intervals: %d\n", total_intervals); // Deterministic work indicator 
+        printf("Function ID: %.10f\n", func_id);
+        printf("Error Tolerance: %.10f\n", tol);
         printf("Execution Time: %f seconds\n", end_time - start_time);
     }
 
